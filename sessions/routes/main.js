128e538ebbb04be13791942d1c62a64b4b88c6eb
Matthew Downes <mddownes@umass.edu>
2014-11-17 19:15:39 -0500

    more deisign

--- sessions/routes/main.js
+++ sessions/routes/main.js
@@ -3,14 +3,37 @@ var router = express.Router();
 
 
 router.get('/', function(req,res){
-	res.render('gameLogin2',{ title : "Guess Who"});
+	res.render('gameLogin2');
 });
 
 router.post('/change', function(req,res){
-	res.redirect('/main/login2');
+	res.redirect('/main/login');
 });
 
-router.get('/login2', function(req,res){
-	res.render('gameLogin', {title : "Guess Who"});
+router.get('/login', function(req,res){
+	res.render('gameLogin');
 });
+
+router.post('/auth', function(req,res){
+	res.redirect('/main/home');
+});
+
+router.get('/home', function(req,res){
+	res.render('home');
+
+});
+
+router.post('/auth1', function(req,res){
+	res.redirect('/main/newuser');
+});
+
+router.get('/newuser', function(req,res){
+	res.render('newuser');
+});
+
+router.get('/game', function(req,res){
+	res.render('game');
+});
+
+
 module.exports = router;
