37d2808e0d5193e16c8a5e67136d11bc33afbb6b
stanleyrya <stanleyrya@gmail.com>
2014-12-02 17:40:20 -0500

    working on pokemon database

--- sessions/db/database-server.js
+++ sessions/db/database-server.js
@@ -6,87 +6,26 @@ var index = require('./index.js');
 var querystring = require('querystring');
 
 function pictureHandler(request, response){ //do we want to return an array/whatever of images??
-  response.writeHead(200, { 'Content-Type' : 'images/png'});
+  response.writeHead(200, { 'Content-Type' : 'text/location'});
 
-  var obj = {
-    host : request.headers.host,
-    url : request.url
-  };
-
-  images = obj;
-  response.write(obj);
-  response.end;
-}
-
-function queryHandler(request, response){
-
-  response.writeHead(200, { 'Content-Type' : 'text/json' });
-  var data;
-
-  var query = querystring.parse(request.url.split('?')[1]); //parse the querystring from the url
-
-  //for use with on "ready". This helps dealing with javascript's asyncronous flow
-  var that = this; //The emitter for "ready" is inside index.js, in each callback function
-
-  switch (query.command) {
-  case 'usa':
-    data = index.getUsersWithAddress(index.jsonData, that);
-    break;
-  case 'ua':
-    data = index.getUserWithAddress(query.fname, query.lname, index.jsonData, that);;
-    break;
-  case 'u':
-    data = index.getUsers(index.jsonData, that);
-    break;
-  case 'n':
-    data = index.getUsers(index.jsonData, that);
-    break;
-  case 'p':
-    data = index.updatePassword(query.fname, query.lname, query.passwd, index.jsonData, that);
-    break;
-  case 'c':
-    data = index.createUser(query.uid, query.fname, query.lname, query.passwd, query.age, index.jsonData, that);
-    break;
-  default:
-    console.log("I don't know what you mean: " + query.command);
-    process.exit(1);
-  }
-
-  this.on('ready', function(){
-    data = index.getRecentRequest(); //to help with asyncronous flow problems. Gets the most recent request to index.js
-    response.write(data);
-    response.end();
-  });
-}
-
-//return javascript array of objects from CSV
-function FileReader(file) {
-  if (file === null) {
-    throw new Error('file must be non-null');
-  }
-
-  if (file === undefined) {
-    throw new Error('file must be defined');
-  }
-
-  this.file = file;
-
+  var images;
   var that = this;
+
+  index.getPictures(new function(err, data){
+    if (err) {
+      throw err;
+    }
+    else {
+      console.log(data);
+      images = data;
+      that.emit('ready');
+  }});
   
-  this.on('read', function () {
-    fs.readFile(file, function (err, data) {
-      var objectArray = [];
-      var allLines = data.toString().split('\n');
-      var headers = allLines[0].toString().split(',');
-      objectArray.push(headers);
-      for(var i=1; i<allLines.length; i++){
-        var line = allLines[i].toString().split(',');
-        var person = { fName : line[0], lName : line[1], uid:line[2], phone:line[3], address:line[4]};
-        objectArray.push(person);
-      }
-      data = objectArray;
-      that.emit('done', data);
-    });
+  this.on('done', function (data) {
+    console.log(images);
+    console.log(JSON.stringify(images));
+    response.write(JSON.stringify(images));
+    response.end;
   });
 }
 
@@ -111,5 +50,5 @@ switch (handlerType) {
     throw new Error('invalid handler type!');
 }
 
-server.listen(4000);
+server.listen(3000);
 console.log('Server is listening!');
--- sessions/db/index.js
+++ sessions/db/index.js
@@ -5,20 +5,24 @@ var connString = 'postgres://student:student@localhost/student'; //this might be
 //'that' refers to the server's queryHandler function. index.js tells that to emit 'ready' when it can get the most recent query
 //this all had to be done to bypass javascript's asyncronous flow "feature"
 
-function getPictures(callback, that){
+function getPictures(callback){
+  console.log("getPictures");
   pg.connect(connString, function(err, client, done){
     if(err){
-      callback(err, undefined, that);
+      console.log("getPictures - if  ");
+      callback(err, undefined);
     }
     else{
+      console.log("getPictures - else  ");
       client.query('select * from pictures P', function(err, result){
         done();
         client.end();
+        console.log("getPictures - else  query");
         if(err){
-          callback(err, undefined, that);
+          callback(err, undefined);
         }
         else{
-          callback(undefined, result.rows, that);
+          callback(undefined, result.rows);
         }
       })
     }
@@ -26,3 +30,14 @@ function getPictures(callback, that){
 }
 
 exports.getPictures = getPictures;
+
+//callback function
+//'data' is just an array of users/objects. Each 'user' in this case has fname, lname, street, zip, etc.....
+function locationHandler(err, data){
+  if (err) {
+    throw err;
+  }
+  else {
+    return data;
+  }
+}
