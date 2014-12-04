60d6dd7b02fad45348658259e43ee3287d5b7d03
Jade <jlhedric@umass.edu>
2014-12-03 21:02:18 -0500

    chat stuff

--- sessions/lib/index.js
+++ sessions/lib/index.js
@@ -8,6 +8,11 @@ app.get('/', function(req, res){
 
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
+  	if(msg === 'Squirtle'){
+  		//io.emit('chat message', 'hi');
+  		
+  		
+  	}
    io.emit('chat message', msg);
   });
 });
